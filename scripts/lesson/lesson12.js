25c1d929966d4124400fb147f60a253fb7258e7e
尹文楷 <yinwk@servyou.com.cn>
2017-11-15 18:38:45 +0800

    add lesson 12

--- scripts/lesson/lesson12.js
+++ scripts/lesson/lesson12.js
@@ -5,7 +5,7 @@
         time: "2017-06-30",
         _r: 370883199306300013
     };
-    let obj_transform = new Proxy(obj, {
+    let ProxyObj = new Proxy(obj, {
         get(target, key) {
             if (key === "time") {
                 return target[key].replace('2017', '1993');
@@ -15,7 +15,9 @@
         },
         set(target, key, value) {
             if (key !== "name") {
-                target[key] = value;
+                return value;
+            } else {
+                return target[key];
             }
         },
         has(target, key) {
@@ -25,20 +27,35 @@
                 return target[key];
             }
         },
+        deleteProperty(target, key) {
+            if (key === "time") {
+                return true;
+            }
+            return target[key];
+        },
         ownKeys(target) {
-
+            return Object.keys(target).filter((filterItem, filterIndex) => {
+                return filterItem.indexOf("_") === -1;
+            });
         }
     });
-    console.log(Reflect.get(obj_transform, 'time'));
-    console.log(Reflect.get(obj_transform, '_r'));
-    console.log(Reflect.get(obj_transform, 'name'));
-    Reflect.set(obj_transform, 'name', 'Simon');
-    Reflect.set(obj_transform, 'time', '2017-12-25');
-    console.log(Reflect.get(obj_transform, 'name'));
-    console.log(Reflect.get(obj_transform, 'time'));
-    console.log(Reflect.has(obj_transform, 'name'));
-    console.log(Reflect.has(obj_transform, '_r'));
-    console.log(Reflect.has(obj_transform, 'time'));
+    console.log(ProxyObj.name, ProxyObj.time, ProxyObj._r);
+    ProxyObj.name = 'Simon';
+    ProxyObj.time = '2017-12-25';
+    console.log(ProxyObj.name, ProxyObj.time);
+    // console.log(Reflect.get(obj_transform, 'time'));
+    // console.log(Reflect.get(obj_transform, '_r'));
+    // console.log(Reflect.get(obj_transform, 'name'));
+    // Reflect.set(obj_transform, 'name', 'Simon');
+    // Reflect.set(obj_transform, 'time', '2017-12-25');
+    // console.log(Reflect.get(obj_transform, 'name'));
+    // console.log(Reflect.get(obj_transform, 'time'));
+    // console.log(Reflect.has(obj_transform, 'name'));
+    // console.log(Reflect.has(obj_transform, '_r'));
+    // console.log(Reflect.has(obj_transform, 'time'));
+    // Reflect.ownKeys(obj_transform).forEach((ownKeysItem, ownKeysIndex) => {
+    //     console.log(ownKeysItem);
+    // });
 }
 
 {
@@ -47,8 +64,8 @@
         return new Proxy(person, {
             set(target, key, value, proxy) {
                 if (target.hasOwnProperty(key)) {
-                    let inspectFunc = inspect[key];
-                    let val = inspectFunc.call(target, value);
+                    let inspectObj = inspect[key];
+                    let val = inspectObj.call(target, value);
                     if (!!val) {
                         return Reflect.set(target, key, value, proxy);
                     } else {
@@ -61,7 +78,7 @@
         });
     }
 
-    let inspectObj = {
+    const inspectResource = {
         name(value) {
             return typeof value === "string";
         },
@@ -71,18 +88,18 @@
     };
 
     class Person {
-        constructor(name = 'yinwenkai', age = 24) {
+        constructor(name = 'yinwk', age = 22) {
             this.name = name;
             this.age = age;
-            return proxyWithReflect(this, inspectObj);
+            return proxyWithReflect(this, inspectResource);
         }
     }
 
     let yinwk = new Person();
     console.log(yinwk.name, yinwk.age);
     // yinwk.name = 18;
-    // yinwk.age = 15;
-    // yinwk.age = "yinwk";
-    yinwk.age = 23;
+    // yinwk.age = 16;
+    // yinwk.age = "zhaoyue";
+    yinwk.age = 20;
     console.log(yinwk.name, yinwk.age);
 }
