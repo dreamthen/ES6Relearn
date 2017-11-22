1cae7b98d1a516673c4cec7531e1fd99fb435e20
dreamthen <dreamthen.00@gmail.com>
2017-11-22 23:30:27 +0800

    add lesson 2 解构赋追

--- scripts/lesson/lesson2.js
+++ scripts/lesson/lesson2.js
@@ -3,9 +3,20 @@
     let arr = [true, "yinwk", 24, {hobby: "program"}, [7, 6, 5, 4, 3, 2, 1]];
     let [, name, age, hobby, arrayNumber] = arr;
     console.log("name:", name, "age:", age, "hobby:", hobby, "array number:", arrayNumber);
+    let arrAno = ["yinwk", 28, 24, 7, 77, 17, 89];
+    let [myName, , myAge, , , luckNumber,] = arrAno;
+    console.log("myName:", myName, "myAge:", myAge, "luckNumber:", luckNumber);
 }
 
 {
-    //对象解构赋值
-    
+    //对象结构赋值
+    let person = {
+        name: "yinwenkai",
+        age: 24,
+        lover: "zhaoyue",
+        hobby: "program"
+    };
+
+    let {name: myName, age: myAge, lover: myLover, hobby: myHobby} = person;
+    console.log("myName:", myName, "myAge:", myAge, "myLover:", myLover, "myHobby:", myHobby);
 }
