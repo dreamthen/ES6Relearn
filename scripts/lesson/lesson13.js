d783ec882b0d91785e2f22f80e4133ef4fadc71a
dreamthen <dreamthen.00@gmail.com>
2017-12-31 21:46:12 +0800

    add 类和对象 lesson

--- scripts/lesson/lesson13.js
+++ scripts/lesson/lesson13.js
@@ -1,1 +1,85 @@
 //类和对象
+{
+    class Person {
+        constructor(name = "yinwk", age = 24) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    // let person = new Person();
+    let person = new Person("Gary", 25);
+    console.log(person["name"], person["age"]);
+}
+
+{
+    class Person {
+        constructor(name = "zhaoyue", age = 24) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    class Gary extends Person {
+        constructor(name, age, hobby = "play computer game") {
+            super(name, age);
+            this.hobby = hobby;
+        }
+    }
+
+    let gary = new Gary("Gary", 25);
+    console.log(gary["name"], gary["age"], gary["hobby"]);
+}
+
+{
+    //静态方法
+    class Person {
+        constructor(name = "xiuhaiyan", age = 52) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    class Gary extends Person {
+        constructor(name, age, hobby = "advertisement") {
+            super(name, age);
+            this.hobby = hobby;
+        }
+
+        sayHello() {
+            console.log("hello I'm", this.name, ",Nice to meet you~ I'm", this.age, "year's old.I love", this.hobby, "!");
+        }
+    }
+
+    let gary = new Gary("yinqingdong", 51);
+    console.log(gary["name"], gary["age"], gary["hobby"]);
+    gary.sayHello();
+}
+
+{
+    //静态属性
+    class Person {
+        constructor(name = "xiuhaiyan", age = 52) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    class Gary extends Person {
+        constructor(name, age, hobby = "advertisement") {
+            super(name, age);
+            this.hobby = hobby;
+        }
+
+        sayHello() {
+            console.log("hello I'm", this.name, ",Nice to meet you~ I'm", this.age, "year's old.I love", this.hobby, "!");
+        }
+    }
+
+    Gary.eyes = "I love blue eye~";
+    Gary.ears = "I love small ears~";
+
+    let gary = new Gary("yinqingdong", 51, "program");
+    console.log(gary["name"], gary["age"], gary["hobby"], Gary.eyes, Gary.ears);
+    gary.sayHello();
+}
