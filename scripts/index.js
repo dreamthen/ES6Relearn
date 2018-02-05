79e2db2c1f7d30eee378a343b69d45976e4ca408
尹文楷 <yinwk@servyou.com.cn>
2018-02-05 10:10:24 +0800

    stylesheet new learn

--- scripts/index.js
+++ scripts/index.js
@@ -38,3 +38,45 @@ import util from "./lesson/lesson18";
 console.log(util.MAX_NUMBER, util.name);
 util.person();
 import "./lesson/aQuery";
+
+var data = [
+    {prop: 1, val: 100, grade: 'A'},
+    {prop: 5, val: 110, grade: 'A'},
+    {prop: 9, val: 114, grade: 'A'},
+    {prop: 4, val: 130, grade: 'B'},
+    {prop: 7, val: 141, grade: 'C'},
+    {prop: 6, val: 122, grade: 'B'},
+    {prop: 8, val: 103, grade: 'A'},
+    {prop: 7, val: 151, grade: 'C'}
+];
+
+function sortByVal(a, b) {
+    return b["val"] - a["val"];
+}
+
+let dataSort = data.sort(sortByVal);
+let map = {},
+    end_arr = [],
+    result_arr = [];
+for (let dataItem of dataSort) {
+    let dataMine = dataItem;
+    if (!map[dataMine.grade]) {
+        end_arr.push({
+            grade: dataMine.grade,
+            data: [dataMine]
+        });
+        map[dataMine.grade] = dataMine;
+    } else {
+        for (let endItem of end_arr) {
+            if (endItem.grade === dataMine.grade) {
+                endItem.data.push(dataMine);
+                break;
+            }
+        }
+    }
+}
+
+for(let endItem of end_arr) {
+    result_arr = [...result_arr, endItem["data"]];
+}
+console.log(result_arr);
